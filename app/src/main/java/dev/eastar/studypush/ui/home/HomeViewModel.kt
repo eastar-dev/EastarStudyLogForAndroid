314226f8394daec8fdb2c7a979f3feba6d4e7855
pluto <pluto@eastandroid.com>
2020-07-01 13:49:31 +0900

    .

--- app/src/main/java/dev/eastar/studypush/ui/home/HomeViewModel.kt
+++ app/src/main/java/dev/eastar/studypush/ui/home/HomeViewModel.kt
@@ -3,14 +3,23 @@ package dev.eastar.studypush.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.eastar.studypush.data.StudyRepository
+import kotlinx.coroutines.launch
 
-class HomeViewModel constructor(val studtData : StudyRepository): ViewModel() {
+//class HomeViewModel constructor(val studtData : StudyRepository): ViewModel() {
+class HomeViewModel constructor(): ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun onLoad(){
+        viewModelScope.launch {
+
+        }
+    }
 
 
 }
