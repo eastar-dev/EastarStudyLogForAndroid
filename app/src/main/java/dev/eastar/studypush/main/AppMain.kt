fe561b95a31c92ee87a8324717179f5a614ccb6d
pluto <pluto@eastandroid.com>
2020-07-06 18:03:27 +0900

    login

--- app/src/main/java/dev/eastar/studypush/main/AppMain.kt
+++ app/src/main/java/dev/eastar/studypush/main/AppMain.kt
@@ -1,8 +1,13 @@
 package dev.eastar.studypush.main
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,9 +16,11 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.AndroidEntryPoint
 import dev.eastar.studypush.R
 import dev.eastar.studypush.databinding.AppMainBinding
+import dev.eastar.studypush.ui.login.Login
 import smart.base.BActivity
 
 @AndroidEntryPoint
@@ -40,8 +47,78 @@ class AppMain : BActivity() {
         startLogin()
     }
 
+
+    companion object {
+        const val REQ_LOGIN = 2000
+    }
+
     private fun startLogin() {
-        // Choose authentication providers
+        startActivityForResult(Intent(mContext, Login::class.java), REQ_LOGIN)
+    }
+
+    private fun startLoginCallback(success: Boolean) {
+        Toast.makeText(mContext, "$success", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQ_LOGIN -> startLoginCallback(data?.extras?.getBoolean("result") ?: false)
+        }
+    }
+
+
+    fun startLogin2() {
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            it.data.toString()
+        }.launch(Intent())
+
+
+
+        registerForActivityResult(
+            object : ActivityResultContract<Unit, FirebaseUser>() {
+                override fun createIntent(context: Context, input: Unit?): Intent {
+                    val providers = arrayListOf(
+                        AuthUI.IdpConfig.EmailBuilder().build(),
+                        AuthUI.IdpConfig.PhoneBuilder().build(),
+                        AuthUI.IdpConfig.GoogleBuilder().build(),
+                        AuthUI.IdpConfig.FacebookBuilder().build(),
+                        AuthUI.IdpConfig.AnonymousBuilder().build(),
+                        AuthUI.IdpConfig.GitHubBuilder().build(),
+                        AuthUI.IdpConfig.TwitterBuilder().build()
+                    )
+
+                    return AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                }
+
+                override fun parseResult(resultCode: Int, intent: Intent?): FirebaseUser? {
+                    val response = IdpResponse.fromResultIntent(intent)
+                    return when (resultCode) {
+                        Activity.RESULT_OK -> FirebaseAuth.getInstance().currentUser
+                        else -> null
+                    }
+                }
+            })
+        {
+            Toast.makeText(this, "$it", Toast.LENGTH_LONG).show()
+        }.launch(Unit)
+
+        //if (PP.userId.isBlank())
+        //    startActivity(Login::class.java)
+    }
+
+    private val launcher: ActivityResultLauncher<Unit> = registerForActivityResult(MonthActivityContract()) {
+        Toast.makeText(this, "$it", Toast.LENGTH_LONG).show()
+    }
+
+
+}
+
+class MonthActivityContract : ActivityResultContract<Unit, FirebaseUser>() {
+    override fun createIntent(context: Context, input: Unit?): Intent {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.PhoneBuilder().build(),
@@ -52,38 +129,18 @@ class AppMain : BActivity() {
             AuthUI.IdpConfig.TwitterBuilder().build()
         )
 
-        // Create and launch sign-in intent
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
-            RC_SIGN_IN
-        )
-        //if (PP.userId.isBlank())
-        //    startActivity(Login::class.java)
+        return AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
-
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                val user = FirebaseAuth.getInstance().currentUser
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-            }
+    override fun parseResult(resultCode: Int, intent: Intent?): FirebaseUser? {
+        val response = IdpResponse.fromResultIntent(intent)
+        return when (resultCode) {
+            Activity.RESULT_OK -> FirebaseAuth.getInstance().currentUser
+            else -> null
         }
-    }
-
-    companion object {
-        private const val RC_SIGN_IN: Int = 2000
     }
 }
