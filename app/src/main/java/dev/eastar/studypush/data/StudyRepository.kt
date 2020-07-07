79935915ab60ff0989fd9b5835e7725d05912849
pluto <pluto@eastandroid.com>
2020-07-07 18:00:49 +0900

    load count 20

--- app/src/main/java/dev/eastar/studypush/data/StudyRepository.kt
+++ app/src/main/java/dev/eastar/studypush/data/StudyRepository.kt
@@ -6,6 +6,6 @@ import dev.eastar.studypush.data.model.StudyItemList
 class StudyRepository(private val dataSource: StudyDataSource) {
     suspend fun getStudyList(): StudyItemList {
         Log.e(dataSource)
-        return dataSource.getStudyItems(3, System.currentTimeMillis())
+        return dataSource.getStudyItems(20, System.currentTimeMillis())
     }
 }
